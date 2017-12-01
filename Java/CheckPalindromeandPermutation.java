1976a3fb92a7bc74ea3c2a3f6997a60b7ab90a57
ziszed29 <32400289+karthikkolathumani@users.noreply.github.com>
2017-12-01 11:08:15 -0800

    Checks if a given string to be a permutation of a palindrome

---
+++ Java/CheckPalindromeandPermutation.java
@@ -0,0 +1,62 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class CheckPalindromeandPermutation {
+	public static void main(String[] args) {
+		String check = "abaaa";
+		
+		System.out.println(isResult(check));
+		
+	}
+
+	private static boolean isResult(String check) {
+		
+		HashMap<Character,Integer> map = new HashMap<>();
+		//Integer a = 1;
+		int countEven = 0;
+		int countOdd = 0;
+		check = check.toLowerCase();
+		for(int i = 0; i<check.length() ; i++) {
+			if(check.charAt(i) != ' ') {
+			
+			Integer count = map.containsKey(check.charAt(i)) ? map.get(check.charAt(i)): 0;
+			map.put(check.charAt(i), count+1);
+			}
+		
+			
+		}
+		for(Entry<Character,Integer> entry : map.entrySet()) {
+			
+			Integer count = entry.getValue();
+			//System.out.println(count);
+			if(count%2==0) {
+				countEven ++;
+			}
+			else {
+				countOdd ++;
+			}
+		}
+		
+		
+		for(Entry<Character,Integer> entry : map.entrySet()) {
+			System.out.println(entry);
+		}
+		return 	checkPal(countEven,countOdd);
+		
+		
+		
+	}
+
+	private static boolean checkPal(int countEven, int countOdd) {
+		System.out.println(countEven+" "+ countOdd);
+		if(countOdd>1) {
+			return false;
+		}
+		
+		
+		return true;
+		
+	}
+	
+	
+}
