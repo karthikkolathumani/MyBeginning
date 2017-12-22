4a61c01dab8b3841b581cb73b1c0156d88545ae0
ziszed29 <32400289+karthikkolathumani@users.noreply.github.com>
2017-12-22 02:56:53 -0800

    Tree node deletion added

--- Java/TreePractice.java
+++ Java/TreePractice.java
@@ -1,101 +1,71 @@
 
 public class TreePractice {
-	
-	public static void main(String[] args) {
-		TreePractice theTree = new TreePractice();
-		theTree.insertRec(10, "k");
-		theTree.insertRec(30, "k");
-		theTree.insertRec(50, "k");
-		theTree.insertRec(70, "k");
-		theTree.insertRec(0, "k");
-		theTree.insertRec(12, "k");
-		theTree.insertRec(14, "k");
-		theTree.insertRec(12, "k");
-		
-		theTree.inOrder();
-		
-		
-		System.out.println(theTree.search(11110));
-	}
-	
 	Node root;
-	
 	class Node{
-		int value;
-		String name;
+		int val;
 		Node left;
 		Node right;
 		
-		public Node(int val, String nam) {
-			value = val;
-			name = nam;
+		public Node(int val) {
+			this.val = val;
+			left = null;
+			right = null;
 		}
-		
 	}
 	
-	TreePractice(){
+	
+	public TreePractice() {
 		root = null;
-		
 	}
 	
-	public boolean insertRec(int val, String nam) {
-		Node n = new Node(val,nam);
-		
+	public boolean insertRec(int val) {
+		Node newNode = new Node(val);
 		if(root==null) {
-			root = n;
+			root = newNode;	
 			return true;
 		}
 		
 		else {
-			Node focusNode = root;
-			Node parent ;
+			Node focusNode;
+			Node parent;
+			focusNode = root;
 			
 			while(true) {
-				 parent = focusNode;
-				if(val<focusNode.value) {
+				parent = focusNode;
+							
+				if(val<=focusNode.val) {
 					focusNode = focusNode.left;
 					if(focusNode==null) {
-						parent.left = n;
+						parent.left = newNode;
 						return true;
 					}
+					
+					
 				}
+				
 				else {
 					focusNode = focusNode.right;
 					if(focusNode==null) {
-						parent.right = n;
+						parent.right = newNode;
 						return true;
 					}
 				}
+				
 			}
-			
-			
 		}
 		
 		
-		
-		
-		}
-	
-	public void inOrder() {
-		inOrderTraversal(root);
 	}
 	
 	
-	public void inOrderTraversal(Node root) {
-		if(!(root==null)) {
-			inOrderTraversal(root.left);
-			System.out.println(root.value);
-			inOrderTraversal(root.right);
-			
-		}
-	}
-	
-	
-	public boolean search(int val) {
-		Node focusNode = root;
-		
-		while(!(focusNode.value==val)) {
-			if(val<focusNode.value) {
+	public Node[] search(int val) {
+		Node focusNode;
+		Node parent = null;
+		Node [] parentChild = new Node[2];
+		focusNode = root;
+		while(focusNode.val!=val) {
+			parent = focusNode;
+			if(val<focusNode.val) {
 				focusNode = focusNode.left;
 			}
 			else {
@@ -103,15 +73,162 @@ public class TreePractice {
 			}
 			
 			if(focusNode==null) {
-				return false;
+				return null;
 			}
+			
 		}
-		return true;
+		if(parent!=null) {
+		parentChild[0] = focusNode;
+		parentChild[1] = parent;
+		}
+		else {
+			parentChild[0] = focusNode;
+			parentChild[1] = null;
+		}
+		if(parent!=null) {
+		System.out.println("Found Node : " + focusNode.val + " with Parent value: "+ parent.val);
+		}
+		else {
+			System.out.println("Single Found Node : " + focusNode.val);
+		}
+		return parentChild;
 	}
 	
 	
+	public boolean deleteNode(int val) {
+	
+		Node delete[] = search(val);
+		Node nodeToDelete = delete[0];
+		Node nodeToDelParent = delete[1];
+		
+		if(nodeToDelParent==null) {
+			System.out.println("Loop 1");
+			Node focus;
+			
+			if(nodeToDelete.left==null && nodeToDelete.right==null) {
+				root=null;
+				System.out.println("Tree is now empty");
+				return true;
+			}
+			
+			if(nodeToDelete.left!=null && nodeToDelete.right==null) {
+				root = root.left;
+				return true;
+			}
+			if(nodeToDelete.right!=null && nodeToDelete.left==null) {
+				root = root.right;
+				return true;
+			}
+			if(nodeToDelete.left!=null && nodeToDelete.right!=null) {
+				focus = nodeToDelete.right;
+				Node Parent = null;
+				while(focus!=null) {
+					Parent =focus;
+					focus = focus.left;
+				}
+				//System.out.println(Parent.val);
+				root.val = Parent.val;
+				root.right =null;
+				
+				return true;
+			}
+			
+
+			
+		}
+		//Node has no children 
+		if(nodeToDelete.left==null && nodeToDelete.right==null) {
+			System.out.println("Loop 2");
+						
+			if(val<nodeToDelParent.val) {
+				nodeToDelParent.left = null;
+			}
+			else {
+				nodeToDelParent.right = null;
+			}
+			return true;
+		}
+		
+		//Node has a single Child
+		
+		if(nodeToDelete.left!=null && nodeToDelete.right==null) {
+			System.out.println("Loop 3");
+			Node focus;
+			focus = nodeToDelete.left;
+			if(val<nodeToDelParent.val) {
+				nodeToDelParent.left = focus;
+			}
+			if(val>nodeToDelParent.val) {
+				nodeToDelParent.right = focus;
+			}
+			return true;
+		}
+		if(nodeToDelete.right!=null && nodeToDelete.left==null) {
+			Node focus;
+			focus = nodeToDelete.right;
+			if(val<nodeToDelParent.val) {
+				nodeToDelParent.left = focus;
+			}
+			if(val>nodeToDelParent.val) {
+				nodeToDelParent.right = focus;
+			}
+			return true;
+		}
+		
+		
+		
+		if(nodeToDelete.left!=null && nodeToDelete.right!=null) {
+			System.out.println("Loop 4");
+			Node focus;
+			Node Parent = null;
+			focus = nodeToDelete.right;
+			while(focus!=null) {
+				Parent =focus;
+				focus = focus.left;
+			}
+			//System.out.println(Parent.val);
+			nodeToDelete.val = Parent.val;
+			Parent =null;
+			//System.out.println(Parent.val);
+			return true;
+
+		}
+
+		
+		return false;
 	}
 	
 	
+	public void inOrder() {
+		inOrderTraversal(root);
+	}
+	
+	public void inOrderTraversal(Node root) {
+		if(root!=null) {
+			inOrderTraversal(root.left);
+			System.out.println(root.val);
+			inOrderTraversal(root.right);
+		}
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		TreePractice tTree = new TreePractice();
+		tTree.insertRec(10);
+		tTree.insertRec(9);
+		tTree.insertRec(1);
+		tTree.insertRec(80);
+		tTree.insertRec(8);
+		tTree.inOrder();
+		
+		//tTree.search(11);
+		
+		tTree.deleteNode(80);
+		tTree.inOrder();
+		
+	}
+	
 
-
+}
