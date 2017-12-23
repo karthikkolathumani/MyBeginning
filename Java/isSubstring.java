da0e4d2e7dc4803db2133c15ad5efa941f170141
ziszed29 <32400289+karthikkolathumani@users.noreply.github.com>
2017-12-22 16:53:12 -0800

    Add files via upload

--- Java/isSubstring.java
+++ Java/isSubstring.java
@@ -1,29 +1,33 @@
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+
 public class isSubstring {
-static String a = "devops";
-static String b = "evo";
+	static String a = "devops";
+	static String b = "ecv";
+	static String pattrn = "(){1,}";
 public static void main(String[] args) {
+	b = pattrn.substring(0, 1)+b+pattrn.substring(1, 6);
 	isSub(a,b);
-	
+
 }
 public static boolean isSub(String a,String b) {
-Pattern pattern  = Pattern.compile("(evo){1,}");
-Matcher match = pattern.matcher(a);
-int start=-1;
-int end=-1;
-while(match.find()) {
-start = match.start();
-end = match.end();
-}
-
-if(start<0 && end<0) {
-	return false;
-}
-System.out.println("Starts at : " + start);
-return true;
-}
+	Pattern pattern  = Pattern.compile(b);
+	Matcher match = pattern.matcher(a);
+	int start=-1;
+	int end=-1;
+	while(match.find()) {
+	start = match.start();
+	end = match.end();
+	}
+	
+	if(start<0 && end<0) {
+		return false;
+	}
+	System.out.println("Starts at : " + start);
+	return true;
+	}
 
 }
 
