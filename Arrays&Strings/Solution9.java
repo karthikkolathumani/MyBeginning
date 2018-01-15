c95af6c17098c08252530a80aa9b3148fe3296a9
ziszed29 <32400289+karthikkolathumani@users.noreply.github.com>
2018-01-15 12:29:21 -0800

    Add files via upload

---
+++ Arrays&Strings/Solution9.java
@@ -0,0 +1,16 @@
+package ArraysAndString;
+
+public class Solution9 {
+	public static void main(String[] args) {
+		String beforeRotation = "Karthik";
+		String afterRotation = "ikKarth";
+		//isRotated(beforeRotation,afterRotation);
+	}
+	/*static boolean isRotated(String a, String b) {
+		if(a.length()!=b.length()) return false;
+		
+		String aa = a+a;
+		return isSubstring(aa,b); // isSubstring is a function provided in the question
+				
+	}*/
+}
